66f64042dfbf8b123c8ab1c463c42cd08840a3d8
Nicolás López Belsué <nlopezbelsue@frba.utn.edu.ar>
2025-12-15 13:10:44 -0300

    Update HechoRepository.java

--- demo/src/main/java/proxy/demo/models/repositories/HechoRepository.java
+++ demo/src/main/java/proxy/demo/models/repositories/HechoRepository.java
@@ -1,8 +1,12 @@
 package proxy.demo.models.repositories;
 
 import org.springframework.stereotype.Repository;
+import proxy.demo.models.Enums.Origen;
+import proxy.demo.models.entities.Categoria;
 import proxy.demo.models.entities.Hecho;
+import proxy.demo.models.entities.Ubicacion;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +14,38 @@ import java.util.Optional;
 @Repository
 public class HechoRepository {
     private final List<Hecho> hechos = new ArrayList<>();
+
+    public HechoRepository() {
+        this.hechos.add(new Hecho(
+                1,
+                Origen.FUENTE_DINAMICA,
+                "Incendio forestal en Córdoba",
+                "Bomberos trabajaron horas para contener un incendio de gran magnitud.",
+                new Categoria("clima"),
+                new Ubicacion(-31.4123, -64.1812),
+                LocalDateTime.now().minusDays(10)
+        ));
+
+        this.hechos.add(new Hecho(
+                2,
+                Origen.FUENTE_DINAMICA,
+                "Viento Zonda en Mendoza",
+                "Ráfagas fuertes provocaron caída de postes y baja visibilidad.",
+                new Categoria("clima"),
+                new Ubicacion(-32.8895, -68.8458),
+                LocalDateTime.now().minusDays(8)
+        ));
+
+        this.hechos.add(new Hecho(
+                3,
+                Origen.FUENTE_DINAMICA,
+                "Inundación en La Plata",
+                "Lluvias intensas causaron anegamientos en varios barrios.",
+                new Categoria("clima"),
+                new Ubicacion(-34.9212, -57.9545),
+                LocalDateTime.now().minusDays(7)
+        ));
+    }
 
     public List<Hecho> findAll() {
         return hechos;
